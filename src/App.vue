ae368baea44cc91beb55121451e37987c5491775
Alberto Taffani <alberto.taffani@gmail.com>
2025-11-04 15:04:57 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue';
+import { pillole } from './Pillole';
+
+const msg = ref(pillole[getDailyRandomNumber()])
+
+function getDailyRandomNumber() {
+  let today = new Date().toISOString().split('T')[0];
+  let hash = 0;
+  for (let i = 0; i < today.length; i++) {
+    hash = today.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const random = Math.abs(hash) % 100;
+  return random;
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="testoPillola">{{ msg }}</div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100dvw;
+  height: 100dvh;
+}
+
+.testoPillola {
+  font-family: Georgia, serif;
+  font-size: x-large;
+  color: white;
+  text-shadow: #14141f 1px 0 10px;
+}
+</style>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+  background-color: #29293d;
+}
+</style>
